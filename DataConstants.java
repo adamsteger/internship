46bc09a16ab3c61f371958fe798389a477ff39e0
Adam Steger <adamsteg24@gmail.com>
2021-10-19 13:57:44 -0400

    Added student constants

--- DataConstants.java
+++ DataConstants.java
@@ -1,13 +1,15 @@
 public abstract class DataConstants {
-    protected static final String USER_FILE_NAME = "users.json";
+    protected static final String USER_FILE_NAME = "student.json";
     protected static final String USER_ID = "id";
-    protected static final String USER_USER_NAME = "userName";
-    protected static final String USER_FIRST_NAME = "firstName";
-    protected static final String USER_LAST_NAME = "lastName";
+    protected static final String USER_USER_NAME = "username";
     protected static final String USER_PASSWORD = "password";
     protected static final String USER_EMAIL = "email";
-    protected static final String USER_PHONE_NUMBER = "phoneNumber";
-    protected static final String USER_ADDRESS = "address";
-    protected static final String USER_GRAD_YEAR = "gradYear";
-
+    protected static final String USER_RATING = "rating";
+    protected static final String STUDENT_FIRST_NAME = "firstName";
+    protected static final String STUDENT_LAST_NAME = "lastName";
+    protected static final String STUDENT_PHONE_NUMBER = "phone";
+    protected static final String STUDENT_ADDRESS = "address";
+    protected static final String STUDENT_GRAD_YEAR = "gradYear";
+    protected static final String STUDENT_GPA = "gpa";
+    protected static final String STUDENT_SHOW_GPA = "showGPA";
 }
