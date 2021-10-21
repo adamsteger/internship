956e0886325a8fbcecd311ae4a0a31f9cafe5c03
Adam Steger <adamsteg24@gmail.com>
2021-10-21 12:50:21 -0400

    Continued working on class

--- DataLoader.java
+++ DataLoader.java
@@ -8,13 +8,14 @@ import org.json.simple.parser.JSONParser;
 
 
 public class DataLoader extends DataConstants {
-    public static ArrayList<Student> getStudents() {
+	
+	public static ArrayList<Student> getStudents() {
         ArrayList<Student> students = new ArrayList<Student>();
 
         try {
-			FileReader reader = new FileReader(USER_FILE_NAME);
+			FileReader reader = new FileReader(STUDENT_FILE_NAME);
 			JSONParser parser = new JSONParser();	
-			JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+			JSONArray peopleJSON = (JSONArray)parser.parse(reader);
 			
 			for(int i=0; i < peopleJSON.size(); i++) {
 				JSONObject personJSON = (JSONObject)peopleJSON.get(i);
@@ -30,9 +31,19 @@ public class DataLoader extends DataConstants {
 				double gpa = (double)personJSON.get(STUDENT_GPA);
 				boolean showGPA = (boolean)personJSON.get(STUDENT_SHOW_GPA);
 				double rating = (double)personJSON.get(USER_RATING);
-
 				
-				students.add(new Student(id, firstName, lastName, userName, password, gradYear, email, address, phone, gpa, showGPA, rating));
+				JSONArray reviewsJSON = (JSONArray)personJSON.get(STUDENT_REVIEWS);
+				ArrayList<Review> reviews = new ArrayList<Review>();
+				for (int j = 0; j < reviewsJSON.size(); j++) {
+					JSONObject reviewJSON = (JSONObject)reviewsJSON.get(j);
+					UUID writerID = UUID.fromString((String)reviewJSON.get(REVIEW_WRITER_ID));
+					int reviewRating = ((Long)reviewJSON.get(REVIEW_RATING)).intValue();
+					String comment = (String)reviewJSON.get(REVIEW_COMMENT);
+
+					reviews.add(new Review(writerID, reviewRating, comment));
+				}
+
+				students.add(new Student(id, firstName, lastName, userName, password, gradYear, email, address, phone, gpa, showGPA, rating, reviews));
 			}
 			
 			return students;
@@ -44,12 +55,49 @@ public class DataLoader extends DataConstants {
 		return null;
     }
 
+	public static ArrayList<Employer> getEmployers() {
+        ArrayList<Employer> employers = new ArrayList<Employer>();
+
+        try {
+			FileReader reader = new FileReader(EMPLOYER_FILE_NAME);
+			JSONParser parser = new JSONParser();	
+			JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+			
+			for(int i=0; i < peopleJSON.size(); i++) {
+				JSONObject personJSON = (JSONObject)peopleJSON.get(i);
+				UUID id = UUID.fromString((String)personJSON.get(USER_ID));
+				String userName = (String)personJSON.get(USER_USER_NAME);
+				String password = (String)personJSON.get(USER_PASSWORD);
+				String email = (String)personJSON.get(USER_EMAIL);
+				double rating = (double)personJSON.get(USER_RATING);
+				String location = (String)personJSON.get(EMPLOYER_LOCATION);
+				String mission = (String)personJSON.get(EMPLOYER_MISSION);
+
+				
+				employers.add(new Employer(id, userName, password, email, rating, location, mission));
+			}
+			
+			return employers;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+    }
+
 	public static void main(String[] args){
 		ArrayList<Student> students = DataLoader.getStudents();
 
-		for(Student student : students){
+		for(Student student : students) {
 			System.out.println(student);
 		}
+		
+		// ArrayList<Employer> employers = DataLoader.getEmployers();
+
+		// for(Employer employer : employers){
+		// 	System.out.println(employer);
+		// }
 	}
 
 	public static ArrayList<InternshipPost> getInternships() {
