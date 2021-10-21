b8a4e0ee35336cd463f3c30a82a4f551eb786df1
Adam Steger <adamsteg24@gmail.com>
2021-10-21 11:39:42 -0400

    Addec constants for employer

--- DataConstants.java
+++ DataConstants.java
@@ -1,10 +1,10 @@
 public abstract class DataConstants {
-    protected static final String USER_FILE_NAME = "student.json";
     protected static final String USER_ID = "id";
     protected static final String USER_USER_NAME = "username";
     protected static final String USER_PASSWORD = "password";
     protected static final String USER_EMAIL = "email";
     protected static final String USER_RATING = "rating";
+    protected static final String STUDENT_FILE_NAME = "student.json";
     protected static final String STUDENT_FIRST_NAME = "firstName";
     protected static final String STUDENT_LAST_NAME = "lastName";
     protected static final String STUDENT_PHONE_NUMBER = "phone";
@@ -12,4 +12,11 @@ public abstract class DataConstants {
     protected static final String STUDENT_GRAD_YEAR = "gradYear";
     protected static final String STUDENT_GPA = "gpa";
     protected static final String STUDENT_SHOW_GPA = "showGPA";
+    protected static final String STUDENT_REVIEWS = "reviews";
+    protected static final String REVIEW_WRITER = "writer";
+    protected static final String REVIEW_RATING = "rating";
+    protected static final String REVIEW_COMMENT = "comment";
+    protected static final String EMPLOYER_FILE_NAME = "employers.json";
+    protected static final String EMPLOYER_LOCATION = "location";
+    protected static final String EMPLOYER_MISSION = "mission";
 }
