200b3bba437752d0d7b1218d6af3fb477087ab99
Adam Steger <adamsteg24@gmail.com>
2021-10-21 12:51:46 -0400

    Added constructor and toString for DataLoader

--- Student.java
+++ Student.java
@@ -16,6 +16,7 @@ public class Student extends User {
     private ArrayList<Review> reviews;
     private ArrayList<InternshipPost> favoritePosts;
     private static UUID id;
+    private EmployerList employerList;
 
     public Student(String username, String password) {
         super(username, password, id);
@@ -23,7 +24,7 @@ public class Student extends User {
     }
 
     public Student(UUID id, String firstName, String lastName, String username, String password, int gradYear,
-                    String email, String address, String phone, double gpa, boolean showGPA, double rating) {
+                    String email, String address, String phone, double gpa, boolean showGPA, double rating, ArrayList<Review> reviews) {
         super();
         this.firstName = firstName;
         this.lastName = lastName;
@@ -36,11 +37,24 @@ public class Student extends User {
         this.gpa = gpa;
         this.showGPA = showGPA;
         this.rating = rating;
+        this.reviews = reviews;
+        employerList = EmployerList.getInstance();
     }
+
     public String toString() {
-        return "Name: " + firstName + " " + lastName + "\nUsername: " + username + "\nPassword: " 
+        String ret = "\nName: " + firstName + " " + lastName + "\nUsername: " + username + "\nPassword: " 
                     + password + "\nGrad Year: " + gradYear + "\nEmail: " + email + "\nPhone: "
-                    + phone + "\nGPA: " + gpa + "\nRating: " + rating;
+                    + phone + "\nGPA: " + gpa + "\nRating: " + rating + "\nReviews: ";
+        for (Review review : reviews) {
+            ret += review + "\n";
+        }
+        return ret;
+    }
+
+    public void addReview(UUID writerID, int rating, String comment) {
+        Employer writer = employerList.getEmployerByID(writerID);
+        Review review = new Review(writer, rating, comment);
+        reviews.add(review);
     }
 
 }
