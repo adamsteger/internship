8adb6767e5245dfeed4bb9570443d57cf87b6911
taaare <adamsanfacon@gmail.com>
2021-10-25 19:51:06 -0400

    added UserList

--- userList.java
+++ userList.java
@@ -1,3 +1,45 @@
-public class UserList{
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class UserList {
+    private static UserList userList;
+    private ArrayList<User> users;
     
+    private UserList() {
+        users = DataLoader.getUsers();
+    }
+
+    public static UserList getInstance() {
+        if(userList == null)
+            userList = new UserList();
+        return userList;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public User geUserByID(UUID id) {
+        User ret = new User();
+        for (User user : users) {
+            if (user.getUUID().equals(id)) {
+                ret = user;
+            }
+        }
+        return ret;
+    }
+
+    public ArrayList<User> addStudent(User user) { 
+        users.add(user);
+        return users;
+    }
+
+    public ArrayList<User> removeStuent(User user) {
+        users.remove(user);
+        return users;
+    }
+
+    public void save() {
+        
+    }
 }
