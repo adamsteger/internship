6a6e89d27fd0f35452312faef871558c7962fbba
taaare <adamsanfacon@gmail.com>
2021-10-25 19:47:37 -0400

    added InternshipApp methods

---
+++ InternshipApplication.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class InternshipApplication{
+    private InternshipList internshipList;
+    private UserList userList;
+    private User user;
+    public InternshipApplication(){
+
+    }
+    public ArrayList<InternshipPost> findInternships(){
+
+    }
+    public ArrayList<InternshipPost> findInternships(String keyword){
+
+    }
+    public User createAccount(String username, String password, String accountType){
+
+    }
+    public User login(String username, String password){
+
+    }
+    public ArrayList<InternshipPost> getFavoriteInternships(){
+
+    }
+    public ArrayList<InternshipPost> getMyInternships(){
+
+    }
+    public boolean addInternship(Employer employer, ContactInfo recruiterContact, String companyName, String posTitle, String description, String location, ArrayList<String> skillReq, String startDate, String endDate, boolean isRemote, boolean isOpen, String language, int lowPay, int highPay, ArrayList<Student> applicants){
+
+    }
+    public void reviewUser(User user, String comment){
+
+    }
+}
