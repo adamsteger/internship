37e50cf57a593191208b03adbb9cea9df977c121
anaboccanfuso <77176581+anaboccanfuso@users.noreply.github.com>
2021-10-27 13:35:30 -0400

    Admin methods

--- Admin.java
+++ Admin.java
@@ -29,17 +29,11 @@ public class Admin extends User {
        internshipList.save();
     }
 
-<<<<<<< HEAD
-    public void deleteStudentReview(Student student, StudentReview review) {
-        student.getReviews().remove(review);
-        //save this somehow in datawriter
-=======
-    public void deleteReview(Review review) {
+    public void deleteReview(User user, Review review) {
         
->>>>>>> f98ea0fbef1b228648c8620521b1241d64d9ecf4
     }
 
-    public void addReview(Review review) {
+    public void addReview(User user, Review review) {
 
     }
 
