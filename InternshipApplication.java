d2c926c116ce89b9ab97836b3de317830670a59d
Adam Steger <asteger@email.sc.edu>
2021-10-27 21:30:28 -0400

    Added Honor Methods

--- InternshipApplication.java
+++ InternshipApplication.java
@@ -163,4 +163,8 @@ public class InternshipApplication{
     public void addCourse(String course, boolean resume) {
         student.getResume().addCourse(course, resume);
     }
+
+    public void addHonor(Honor honor) {
+        student.getResume().addHonor(honor);
+    }
 }
