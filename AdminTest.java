05c2a4a580daa6a249b62c57ec6a9ef739fc8090
Ana Boccanfuso <boccanfa@email.sc.edu>
2021-11-10 15:26:34 -0800

    Test additions

---
+++ AdminTest.java
@@ -0,0 +1,36 @@
+import org.junit.*;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class AdminTest {
+    
+    @Before
+    public void setup() {
+        
+    }
+
+    @After 
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void testAddPost() {
+
+    }
+    
+    @Test
+    public void testDeletePost() {
+
+    }
+
+    @Test
+    public void testDeleteStudentReview() {
+
+    }
+
+    @Test
+    public void testDeleteEmployerReview() {
+
+    }
+}
